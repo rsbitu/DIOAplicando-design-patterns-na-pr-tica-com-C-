3985a11762ffabe71c8c340e4adadf1b4e789c01
Rafael Bitu <rsbitu@hotmail.com>
2021-01-14 12:23:46 -0300

    first commit

---
+++ src/Infra.Repository.Detran/DetranPBVerificadorDebitosRepository.cs
@@ -0,0 +1,31 @@
+﻿using DesignPatternSamples.Application.DTO;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternSamples.Infra.Repository.Detran
+{
+    public class DetranPBVerificadorDebitosRepository : DetranVerificadorDebitosRepositoryCrawlerBase
+    {
+        private readonly ILogger _Logger;
+
+        public DetranPBVerificadorDebitosRepository(ILogger<DetranPBVerificadorDebitosRepository> logger)
+        {
+            _Logger = logger;
+        }
+
+        protected override Task<IEnumerable<DebitoVeiculo>> PadronizarResultado(string html)
+        {
+            _Logger.LogDebug($"Padronizando o Resultado {html}.");
+            return Task.FromResult<IEnumerable<DebitoVeiculo>>(new List<DebitoVeiculo>() { new DebitoVeiculo() });
+        }
+
+        protected override Task<string> RealizarAcesso(Veiculo veiculo)
+        {
+            _Logger.LogDebug($"Consultando débitos do veículo placa {veiculo.Placa} para o estado de PB.");
+            return Task.FromResult("CONTEUDO DO SITE DO DETRAN/PB");
+        }
+    }
+}
